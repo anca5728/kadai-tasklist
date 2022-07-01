872b51868d0b33a3bb999786e5f1839f102f5c0c
ayaka.kitagawa <a1579aktgw@gmail.com>
2022-07-01 18:42:10 +0900

    Lesson16 課題修正

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -59,7 +59,7 @@ public class CreateServlet extends HttpServlet {
                 request.setAttribute("task", m);
                 request.setAttribute("errors", errors);
 
-                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/task/new.jsp");
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/new.jsp");
                 rd.forward(request, response);
             } else {
                 // データベースに保存
